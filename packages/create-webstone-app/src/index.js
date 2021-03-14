14880de4cc08194d5d515eb30d015c99c87b48f4
Mike Nikles <mikenikles@gmail.com>
2021-03-14 14:42:14 +0000

    feat(create-webstone-app): Copy the template directory.

--- packages/create-webstone-app/src/index.js
+++ packages/create-webstone-app/src/index.js
@@ -1,41 +1,47 @@
-import fs from "fs";
+import fs from "fs-extra";
 import path from 'path';
 import prompts from "prompts";
 
+const pipe = (...functions) => input => functions.reduce((chain, func) => chain.then(func), Promise.resolve(input));
+
+const displayWelcome = () => new Promise((resolve) => {
+  // TODO: Print ASCII art (Webstone logo) and a welcome message
+  resolve();
+});
+
 const createAppDir = async (appName = process.argv[2]) => {
+  console.log(`Creating app directory...`);
   const appDir = appName ? appName.toLowerCase().replace(/\s/g, "-") : ".";
 
   if (fs.existsSync(appDir)) {
     if (fs.readdirSync(appDir).length > 0) {
       const response = await prompts({
-				type: 'confirm',
-				name: 'value',
-				message: `The ./${appDir} directory is not empty. Do you want to overwrite it?`,
-				initial: false
-			});
+        type: 'confirm',
+        name: 'value',
+        message: `The ./${appDir} directory is not empty. Do you want to overwrite it?`,
+        initial: false
+      });
 
-			if (!response.value) {
-				process.exit(1);
-			}
+      if (!response.value) {
+        process.exit(1);
+      }
 
       fs.rmSync(appDir, { recursive: true, force: true });
     }
   }
-  fs.mkdirSync(appDir, {recursive: true});
+  fs.mkdirSync(appDir, { recursive: true });
   return appDir;
 };
 
 const copyTemplate = (appDir) => {
+  console.log(`Copying template...`);
   const templateDir = path.join(__dirname, "template");
-
-  console.log(`TODO: Copy ${templateDir} to ${appDir}`);
+  fs.copySync(templateDir, appDir);
+  return appDir;
 };
 
-const main = async () => {
-  // TODO: Print ASCII art (Webstone logo) and a welcome message
-
-  const appDir = await createAppDir();
-  copyTemplate(appDir);
-};
-
-main();
+pipe(
+  displayWelcome,
+  createAppDir,
+  copyTemplate
+)();
