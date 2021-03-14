bb6046df131d5766915ecaa9eaa0e53caf1c0036
Mike Nikles <mikenikles@gmail.com>
2021-03-14 02:39:05 +0000

    feat(create-webstone-app): Create the app directory; add a placeholder to copy the template.

--- packages/create-webstone-app/src/index.js
+++ packages/create-webstone-app/src/index.js
@@ -1,5 +1,41 @@
-const main = () => {
-  console.log("Test")
+import fs from "fs";
+import path from 'path';
+import prompts from "prompts";
+
+const createAppDir = async (appName = process.argv[2]) => {
+  const appDir = appName ? appName.toLowerCase().replace(/\s/g, "-") : ".";
+
+  if (fs.existsSync(appDir)) {
+    if (fs.readdirSync(appDir).length > 0) {
+      const response = await prompts({
+				type: 'confirm',
+				name: 'value',
+				message: `The ./${appDir} directory is not empty. Do you want to overwrite it?`,
+				initial: false
+			});
+
+			if (!response.value) {
+				process.exit(1);
+			}
+
+      fs.rmSync(appDir, { recursive: true, force: true });
+    }
+  }
+  fs.mkdirSync(appDir, {recursive: true});
+  return appDir;
+};
+
+const copyTemplate = (appDir) => {
+  const templateDir = path.join(__dirname, "template");
+
+  console.log(`TODO: Copy ${templateDir} to ${appDir}`);
+};
+
+const main = async () => {
+  // TODO: Print ASCII art (Webstone logo) and a welcome message
+
+  const appDir = await createAppDir();
+  copyTemplate(appDir);
 };
 
 main();
