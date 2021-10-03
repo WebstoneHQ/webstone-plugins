42c0d8bcf745ca4afdbead8f9fbebd56252789ef
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2021-10-02 21:38:34 -0700

    chore(cli): use Gluegun as the Webstone CLI toolkit (#57)
    * chore(cli): use Gluegun as the Webstone CLI toolkit
    
    * chore(cli): include the necessary files for the npm package

---
+++ packages/cli/__tests__/cli-integration.test.ts
@@ -0,0 +1,17 @@
+const { system, filesystem } = require("gluegun");
+const pkg = require("../package.json");
+
+const src = filesystem.path(__dirname, "..");
+
+const cli = async (cmd) =>
+  system.run("node " + filesystem.path(src, "bin") + ` ${cmd}`);
+
+test("outputs version", async () => {
+  const output = await cli("--version");
+  expect(output).toContain(pkg.version);
+});
+
+test("outputs help", async () => {
+  const output = await cli("--help");
+  expect(output).toContain(pkg.version);
+});
