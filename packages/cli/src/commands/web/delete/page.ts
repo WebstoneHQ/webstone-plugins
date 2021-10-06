3b2355c76649f92428963e7b83e0a6d6bf8085c6
Mike Nikles <mikenikles@pm.me>
2021-10-06 13:41:47 +0000

    feat(cli): add a `webstone web delete page` CLI command
    Add a `webstone web delete page` CLI command.
    
    close #76

---
+++ packages/cli/src/commands/web/delete/page.ts
@@ -0,0 +1,30 @@
+import { GluegunCommand } from "gluegun";
+
+const command: GluegunCommand = {
+  run: async (toolbox) => {
+    const { filesystem, parameters, print, prompt, strings } = toolbox;
+
+    let name = parameters.first;
+    if (!name) {
+      const result = await prompt.ask({
+        type: "input",
+        name: "name",
+        message: `What's the page to delete?`,
+      });
+      if (result && result.name) name = result.name;
+    }
+
+    if (!name) {
+      print.error("Please provide a page, e.g. 'about-us'.");
+      return;
+    }
+
+    const filename = strings.kebabCase(name);
+    const target = `services/web/src/routes/${filename}`;
+    const spinner = print.spin(`Removing page "${target}"...`);
+    filesystem.remove(target);
+    spinner.succeed(`Page deleted at: ${target}`);
+  },
+};
+
+module.exports = command;
