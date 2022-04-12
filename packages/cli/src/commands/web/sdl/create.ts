56517755c90aa7f4577b9c456a92678e4f3f0ca0
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-04-12 17:24:53 +0000

    feat(generate sdl): add template for generated sdl file

--- packages/cli/src/commands/web/sdl/create.ts
+++ packages/cli/src/commands/web/sdl/create.ts
@@ -5,7 +5,7 @@ import { getModel, getQuerySDL } from "../../../lib/sdl-helpers";
 const command: GluegunCommand = {
   description: "Generate GraphQL Schema Definition for a Prisma Schema",
   run: async (toolbox: Toolbox) => {
-    const { parameters, print, prompt } = toolbox;
+    const { parameters, print, prompt, template, strings } = toolbox;
 
     let name = parameters.first;
     if (!name) {
@@ -30,8 +30,19 @@ const command: GluegunCommand = {
       return;
     }
 
-    const querySDL = getQuerySDL(schema).join("\n");
-    console.log(querySDL);
+    const querySDL = getQuerySDL(schema).join("\n\t");
+    const filename = strings.kebabCase(name);
+    const target = `services/data/sdl/${filename}.ts`;
+
+    await template.generate({
+      template: "web/new-sdl.ejs",
+      target,
+      props: {
+        name: strings.pascalCase(name),
+        query: querySDL,
+      },
+    });
+
     spinner.succeed(`SDL for ${name} generated.`);
   },
 };
