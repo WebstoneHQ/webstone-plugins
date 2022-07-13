74a8bdf7250cdb720d23117476cae30ec8fe896b
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-07-13 08:41:24 +0000

    test(test-framework): adjust config & dotfiles

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,9 @@ import { type PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   use: {
     baseURL: "http://localhost:3000",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
+  outputDir: "tests/e2e/test-results",
 };
 export default config;
