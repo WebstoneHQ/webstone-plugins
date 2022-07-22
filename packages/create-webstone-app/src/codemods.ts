83a72dc2f29e40e3310b80dbaa4713529f7d43eb
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-07-22 14:07:55 +0000

    test(test-setup): setup jscodeshift

---
+++ packages/create-webstone-app/src/codemods.ts
@@ -0,0 +1,23 @@
+import { API, FileInfo } from "jscodeshift";
+
+export const parser = "tsx";
+
+export default function adjustPlaywright(file: FileInfo, api: API) {
+  const j = api.jscodeshift;
+
+  return j(file.source)
+    .find(j.ObjectExpression)
+    .forEach((path) => {
+      if (path.value.properties.length === 1) {
+        console.log(path.value.properties);
+        path.value.properties.push({
+          type: "Property",
+          key: { type: "Identifier", name: "testMatch" },
+          value: { type: "Literal", value: ".*e2e.(js|ts|mjs)" },
+          kind: "init",
+        });
+      }
+      return path;
+    })
+    .toSource();
+}
