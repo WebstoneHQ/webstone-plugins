466c8953c2a81717693719c92c74cda97909dd45
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-09-22 17:23:40 +0000

    refactor(create-webstone-app): remove promopt for readme

--- packages/create-webstone-app/src/tasks/3-configure-app/index.ts
+++ packages/create-webstone-app/src/tasks/3-configure-app/index.ts
@@ -2,23 +2,11 @@ import fs from "fs-extra";
 import { ListrTask } from "listr2";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import { Ctx, WebstoneTask } from "../../helpers";
+import { Ctx } from "../../helpers";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export const copyReadme = async (ctx: Ctx, task: WebstoneTask) => {
-  const hasReadme = fs.existsSync(`${ctx.appDir}/README.md`);
-  if (hasReadme) {
-    const response = await task.prompt({
-      type: "confirm",
-      message: `The ./${ctx.appDir} directory already contains a README. Do you want to overwrite it?`,
-      initial: false,
-    });
-    if (!response) {
-      task.skip("Readme not overwritten");
-      return;
-    }
-  }
+export const copyReadme = async (ctx: Ctx) => {
   const templateDir = path.join(__dirname, "..", "template/README.md");
   fs.copySync(templateDir, `${ctx.appDir}/README.md`);
 };
