ce7fa80b71271f8bc4e2a8fa35ce43efc4963107
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-09-25 08:23:41 +0000

    feat(create-webstone-app): install cli

--- packages/create-webstone-app/src/tasks/3-configure-app/index.ts
+++ packages/create-webstone-app/src/tasks/3-configure-app/index.ts
@@ -1,3 +1,4 @@
+import { execa } from "execa";
 import fs from "fs-extra";
 import { ListrTask } from "listr2";
 import path, { dirname } from "path";
@@ -5,6 +6,15 @@ import { fileURLToPath } from "url";
 import { Ctx } from "../../helpers";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const installCLI = async (ctx: Ctx) => {
+  const installProcess = execa("pnpm", ["add", "-D", "@webstone/cli"], {
+    shell: true,
+    cwd: ctx.appDir,
+  });
+
+  return installProcess.stdout;
+};
 
 export const copyReadme = async (ctx: Ctx) => {
   const templateDir = path.join(__dirname, "..", "template", "README.md");
@@ -15,6 +25,10 @@ const configureApp: ListrTask[] = [
   {
     task: copyReadme,
     title: "Configuring Readme",
+  },
+  {
+    task: installCLI,
+    title: "Install Webstone CLI",
   },
 ];
 
