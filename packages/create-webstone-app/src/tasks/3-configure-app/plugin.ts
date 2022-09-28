bbb9d4aff195f66d5a0fde981790550228a1fbd1
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-09-28 17:52:39 +0000

    feat(create-webstone-app): modify tsconfig

--- packages/create-webstone-app/src/tasks/3-configure-app/plugin.ts
+++ packages/create-webstone-app/src/tasks/3-configure-app/plugin.ts
@@ -4,6 +4,7 @@ import { ListrTask } from "listr2";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { Ctx, determinePackageManager } from "../../helpers";
+import json5 from "json5";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -34,9 +35,24 @@ export const copyFiles = async (ctx: Ctx) => {
 };
 
 export const adjustConfigFiles = async (ctx: Ctx) => {
+  //package.json
   const packageJson = await fs.readJSON(`${ctx.appDir}/package.json`);
   packageJson.scripts.build = `${packageJson.scripts.build} && tsc -p tsconfig.cli.json`;
-  return await fs.writeJSON(`${ctx.appDir}/package.json`, packageJson);
+  await fs.writeJSON(`${ctx.appDir}/package.json`, packageJson, { spaces: 2 });
+
+  //tsconfig
+  const tsconfig = await fs.readFile(`${ctx.appDir}/tsconfig.json`, "utf-8");
+
+  const tsconfigJson = json5.parse(tsconfig);
+  console.log({ tsconfigJson });
+  tsconfigJson.exclude = [
+    ...["./node_modules/**", "./svelte-kit/[!ambient.d.ts]**"],
+    "./src/lib/cli/**/*",
+  ];
+  await fs.writeFile(
+    `${ctx.appDir}/tsconfig.json`,
+    JSON.stringify(tsconfigJson, null, 2)
+  );
 };
 
 export const configurePlugin: ListrTask[] = [
