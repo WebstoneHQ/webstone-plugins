51842787251b530b107667c0a82a65aa57616a8a
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-09-29 08:10:03 +0000

    feat(create-webstone-app): argument parsing

--- packages/create-webstone-app/src/tasks/0-determine-context/index.ts
+++ packages/create-webstone-app/src/tasks/0-determine-context/index.ts
@@ -1,13 +1,34 @@
 import { ListrTask } from "listr2/dist/index";
+import CommandLineArgs from "command-line-args";
 
 import { Ctx, WebstoneTask } from "../../helpers";
 
+const optionDefinitons: CommandLineArgs.OptionDefinition[] = [
+  {
+    name: "type",
+    alias: "t",
+    type: String,
+  },
+];
+
 const getMetadata = async (ctx: Ctx, task: WebstoneTask) => {
-  const type: "application" | "plugin" = await task.prompt({
-    type: "Select",
-    message: "What's the type of the project",
-    choices: ["application", "plugin"],
+  let type: "application" | "plugin";
+  const optionDefinitions = CommandLineArgs(optionDefinitons, {
+    partial: true,
   });
+
+  if (
+    optionDefinitions.type === "application" ||
+    optionDefinitions.type === "plugin"
+  ) {
+    type = optionDefinitions.type;
+  } else {
+    type = await task.prompt({
+      type: "Select",
+      message: "What's the type of the project",
+      choices: ["application", "plugin"],
+    });
+  }
 
   let extendCLI = false;
   if (type === "plugin") {
