50b7274b5c3af3829d64ef41370b75ffbf6139d3
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-11-23 17:28:43 +0000

    feat: generate subrouter

--- packages/plugin-trpc/cli/src/commands/trpc/generate.ts
+++ packages/plugin-trpc/cli/src/commands/trpc/generate.ts
@@ -8,37 +8,55 @@ const command: GluegunCommand = {
 	hidden: false,
 	dashed: false,
 	run: async (toolbox) => {
-		const { print, parameters, prompt } = toolbox;
+		const { print, parameters, prompt, strings, template } = toolbox;
+		try {
+			const allModels = parsePrismaSchema();
 
-		const allModels = parsePrismaSchema();
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			let modelName = parameters.first!;
 
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		let modelName = parameters.first!;
+			if (!modelName) {
+				const modelNames = allModels.declarations
+					.filter((declaration) => declaration.kind === 'model')
+					.map((model) => model.kind === 'model' && model.name.value);
 
-		if (!modelName) {
-			const modelNames = allModels.declarations
-				.filter((declaration) => declaration.kind === 'model')
-				.map((model) => model.kind === 'model' && model.name.value);
+				if (!modelNames) {
+					print.error('No models found in prisma/schema.prisma');
+					return;
+				}
 
-			if (!modelNames) {
-				print.error('No models found in prisma/schema.prisma');
+				const result = await prompt.ask({
+					type: 'select',
+					name: 'model',
+					message: 'Please select model',
+					choices: modelNames as string[]
+				});
+				modelName = result.model;
+			}
+
+			const model = getPrismaModelByName(modelName, allModels);
+
+			if (!model) {
+				print.error(`Model ${modelName} not found`);
 				return;
 			}
 
-			const result = await prompt.ask({
-				type: 'select',
-				name: 'model',
-				message: 'Please select model',
-				choices: modelNames as string[]
+			const spinner = print.spin('Generating model');
+
+			await template.generate({
+				template: 'subrouter.ejs',
+				target: `src/lib/server/trpc/subrouters/${strings.snakeCase(
+					strings.singular(modelName)
+				)}.ts`,
+				props: {
+					capitalizedPlural: strings.upperFirst(strings.plural(modelName)),
+					capitalizedSingular: strings.upperFirst(strings.singular(modelName))
+				}
 			});
-			modelName = result.model;
-		}
 
-		const model = getPrismaModelByName(modelName, allModels);
-
-		if (!model) {
-			print.error(`Model ${modelName} not found`);
-			return;
+			spinner.succeed('Generated model');
+		} catch (error) {
+			print.error(error);
 		}
 	}
 };
