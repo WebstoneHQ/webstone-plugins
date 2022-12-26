ebae01913c52590f239f8f4dad4c50bb50ad4361
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-12-26 08:16:16 +0000

    fix: remove patch

--- .gitpod/tasks/2-app-dev/command.sh
+++ .gitpod/tasks/2-app-dev/command.sh
@@ -4,6 +4,4 @@ set -e
 cd webstone-dev-app
 while [ ! -f ../webstone/packages/cli/dist/bin.js ]; do sleep 1; done
 pnpm add -D ../webstone/packages/cli
-export HMR_HOST=`gp url 5173`
-pnpm ws dev gitpod web-patch-svelte-config-js
 pnpm ws dev
