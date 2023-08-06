024a04a451a71a9c1c3ed0682f45df660ec47025
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2023-08-05 23:31:44 -0700

    Add a new --types option when creating a route; fix tests; add changeset.

--- tests/e2e/1-web-pages/create-and-delete.spec.ts
+++ tests/e2e/1-web-pages/create-and-delete.spec.ts
@@ -1,12 +1,13 @@
 import { expect, test } from "@playwright/test";
 import { execSync } from "child_process";
 import { resolve } from "path";
+import { sleep } from "../globals";
 
 const devAppPath = resolve("./_dev-app");
 
 test.describe("web/page/create & web/page/delete", () => {
   test("creates and deletes an About Us page", async ({ page }) => {
-    execSync("pnpm ws web route create 'About Us'", {
+    execSync("pnpm ws web route create 'About Us' --types '+page.svelte'", {
       cwd: devAppPath,
     });
     await page.goto("/about-us");
@@ -15,9 +16,12 @@ test.describe("web/page/create & web/page/delete", () => {
     execSync("pnpm ws web route delete 'About Us'", {
       cwd: devAppPath,
     });
+
+    // The previous `execSync` call to delete /about-us results in a dev server restart.
+    // Let's wait a tiny bit for the restart to complete.
+    await sleep(300);
+
     await page.goto("/about-us");
-    await expect(page.locator("pre").nth(0)).toContainText(
-      "Not found: /about-us"
-    );
+    await expect(page.locator("h1")).toContainText("404");
   });
 });
