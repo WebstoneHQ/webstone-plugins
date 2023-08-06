c7c9aa3c95a1cf01d0f841b3899be37e52d70c4a
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2023-08-06 00:04:01 -0700

    Upgrade create-webstone-app dependencies.

--- packages/create-webstone-app/scripts/build.js
+++ packages/create-webstone-app/scripts/build.js
@@ -4,37 +4,41 @@ import { nodeExternalsPlugin } from "esbuild-node-externals";
 /**
  * @type {Object.<string, esbuild.BuildOptions>}
  */
-
 const config = {
-  shared: {
-    format: "esm",
-    target: "esnext",
-    plugins: [nodeExternalsPlugin()],
-    banner: {
-      js: "#!/usr/bin/env node",
-    },
-    bundle: true,
-    entryPoints: ["./src/index.ts"],
-    logLevel: "info",
-    minify: true,
-    outfile: "./dist/index.js",
-    platform: "node",
+  format: "esm",
+  target: "esnext",
+  plugins: [nodeExternalsPlugin()],
+  banner: {
+    js: "#!/usr/bin/env node",
   },
-  build: {},
-  dev: {
-    watch: true,
-  },
+  bundle: true,
+  entryPoints: ["./src/index.ts"],
+  logLevel: "info",
+  minify: true,
+  outfile: "./dist/index.js",
+  platform: "node",
 };
 
+/**
+ * @type {"build" | "dev"}
+ */
 const mode = process.argv[2];
 if (!mode) {
   console.error("Usage: node ./scripts/esbuild.js build|dev");
   process.exit(1);
 }
 
-esbuild
-  .build({
-    ...config.shared,
-    ...(config[mode] || {}),
-  })
-  .catch(() => process.exit(1));
+switch (mode) {
+  case "build":
+    await esbuild.build(config).catch(() => process.exit(1));
+    break;
+  case "dev": {
+    const context = await esbuild
+      .context({
+        ...config.shared,
+      })
+      .catch(() => process.exit(1));
+    await context.watch();
+    break;
+  }
+}
