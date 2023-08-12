c959a38f4972943a8555d4096baa08cf439327b7
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-08-12 22:23:15 +0000

    feat: init copy function

--- packages/create-webstone-app/src/index.ts
+++ packages/create-webstone-app/src/index.ts
@@ -1,3 +1,6 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import fs from "fs-extra";
 import { CreateWebstoneOptions, WebstoneAppType } from "../types";
 
 export async function createWebstone(
@@ -7,6 +10,7 @@ export async function createWebstone(
   const { type } = options;
   const appName = getAppName(cwd, type);
   console.log("appName", appName);
+  copyTemplate(cwd, type);
 }
 
 function getAppName(cwd: string, type: WebstoneAppType) {
@@ -28,4 +32,20 @@ function toValidPackageName(name: string) {
     .replace(/\s+/g, "-")
     .replace(/^[._]/, "")
     .replace(/[^a-z0-9~.-]+/g, "-");
+}
+
+function copyTemplate(cwd: string, type: WebstoneAppType) {
+  const templateDir = path.resolve(
+    fileURLToPath(import.meta.url),
+    "../..",
+    "templates",
+    type,
+    "structure",
+  );
+  const files = fs.readdirSync(templateDir);
+  for (const file of files.filter((f) => f !== "package.json")) {
+    const targetPath = path.join(cwd, file);
+
+    console.log(targetPath);
+  }
 }
