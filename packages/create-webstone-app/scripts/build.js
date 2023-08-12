9177b4e251971c423f1614833443e8e5c2eadda6
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-08-12 15:44:51 +0000

    refactor: move build to tsup

--- packages/create-webstone-app/scripts/build.js
+++ packages/create-webstone-app/scripts/build.js
@@ -1,40 +1,24 @@
-import esbuild from "esbuild";
-import { nodeExternalsPlugin } from "esbuild-node-externals";
-
-/**
- * @type {Object.<string, esbuild.BuildOptions>}
- */
-const config = {
-  format: "esm",
-  target: "esnext",
-  plugins: [nodeExternalsPlugin()],
-  banner: {
-    js: "#!/usr/bin/env node",
-  },
-  bundle: true,
-  entryPoints: ["./src/index.ts"],
-  logLevel: "info",
-  minify: true,
-  outfile: "./dist/index.js",
-  platform: "node",
-};
+import { defineConfig, build } from "tsup";
 
 /**
  * @type {"build" | "dev"}
  */
+
 const mode = process.argv[2];
-if (!mode) {
-  console.error("Usage: node ./scripts/esbuild.js build|dev");
+
+// check if mode is valid
+if (!["build", "dev"].includes(mode)) {
+  console.log("Invalid mode");
   process.exit(1);
 }
 
-switch (mode) {
-  case "build":
-    await esbuild.build(config).catch(() => process.exit(1));
-    break;
-  case "dev": {
-    const context = await esbuild.context(config).catch(() => process.exit(1));
-    await context.watch();
-    break;
-  }
-}
+const config = defineConfig({
+  entry: ["src/index.ts"],
+  target: "esnext",
+  format: "esm",
+  treeshake: true,
+  minify: true,
+  watch: mode === "dev" ? ["src"] : false,
+});
+
+await build(config);
