057e733972503012d0e2ead7187128b4aba9c8df
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-08-17 18:13:19 +0000

    test: add new tests

--- packages/create-webstone-app/src/functions.spec.ts
+++ packages/create-webstone-app/src/functions.spec.ts
@@ -1,12 +1,16 @@
 import { it, describe } from "node:test";
-import { deepStrictEqual } from "node:assert";
+import { deepEqual, deepStrictEqual, strictEqual } from "node:assert";
+import path from "node:path";
 import {
   sortKeys,
   toValidPackageName,
   getAppName,
   getRawAppName,
+  updatePackageJSON,
+  copyBuildScript,
 } from "./functions";
 import { WebstoneAppType } from "../types";
+import fs from "fs-extra";
 
 describe("sortKeys", () => {
   it("should sort keys of an object in ascending order", () => {
@@ -131,5 +135,126 @@ describe("getRawAppName", () => {
     const expectedOutput = "/a";
     const result = getRawAppName(cwd);
     deepStrictEqual(result, expectedOutput);
+  });
+});
+
+describe("updatePackageJson", () => {
+  it("should update the package.json properly for type app", (ctx) => {
+    const mockReadJsonSync = ctx.mock.fn(fs.readJSONSync, () => {
+      return {};
+    });
+    const mockWriteJsonSync = ctx.mock.fn(fs.writeJsonSync, () => {});
+    const mockPathResolve = ctx.mock.fn(path.resolve, (cwd: string) => cwd);
+    ctx.mock.method(fs, "readJSONSync", mockReadJsonSync);
+    ctx.mock.method(fs, "writeJsonSync", mockWriteJsonSync);
+    ctx.mock.method(path, "resolve", mockPathResolve);
+    const cwd = "dummy";
+    updatePackageJSON(cwd, { extendCLI: false, type: "app" });
+    strictEqual(mockReadJsonSync.mock.callCount(), 1);
+    strictEqual(mockWriteJsonSync.mock.callCount(), 1);
+    deepStrictEqual(mockWriteJsonSync.mock.calls[0].arguments, [
+      "dummy/package.json",
+      {
+        devDependencies: {
+          "@webstone/cli": "^0.12.0",
+        },
+      },
+      {
+        encoding: "utf-8",
+        spaces: "\t",
+      },
+    ]);
+  });
+
+  it("should update the package.json properly for type plugin that does extend the cli", (ctx) => {
+    const mockReadJsonSync = ctx.mock.fn(fs.readJSONSync, () => {
+      return {};
+    });
+    const mockWriteJsonSync = ctx.mock.fn(fs.writeJsonSync, () => {});
+    const mockPathResolve = ctx.mock.fn(path.resolve, (cwd: string) => cwd);
+    ctx.mock.method(fs, "readJSONSync", mockReadJsonSync);
+    ctx.mock.method(fs, "writeJsonSync", mockWriteJsonSync);
+    ctx.mock.method(path, "resolve", mockPathResolve);
+    const cwd = "dummy";
+    updatePackageJSON(cwd, { extendCLI: false, type: "plugin" });
+    strictEqual(mockReadJsonSync.mock.callCount(), 1);
+    strictEqual(mockWriteJsonSync.mock.callCount(), 1);
+    deepStrictEqual(mockWriteJsonSync.mock.calls[0].arguments, [
+      "dummy/package.json",
+      {},
+      {
+        encoding: "utf-8",
+        spaces: "\t",
+      },
+    ]);
+  });
+
+  it("should update the package.json properly for type plugin that doesn't extend the cli", (ctx) => {
+    const mockReadJsonSync = ctx.mock.fn(fs.readJSONSync, () => {
+      return {};
+    });
+    const mockWriteJsonSync = ctx.mock.fn(fs.writeJsonSync, () => {});
+    const mockPathResolve = ctx.mock.fn(path.resolve, (cwd: string) => cwd);
+    ctx.mock.method(fs, "readJSONSync", mockReadJsonSync);
+    ctx.mock.method(fs, "writeJsonSync", mockWriteJsonSync);
+    ctx.mock.method(path, "resolve", mockPathResolve);
+    const cwd = "dummy";
+    updatePackageJSON(cwd, { extendCLI: true, type: "plugin" });
+    strictEqual(mockReadJsonSync.mock.callCount(), 1);
+    strictEqual(mockWriteJsonSync.mock.callCount(), 1);
+    deepStrictEqual(mockWriteJsonSync.mock.calls[0].arguments, [
+      "dummy/package.json",
+      {
+        scripts: {
+          build:
+            "npm run clean:build && npm run cli:build && npm run web:build",
+          "clean:build": "rimraf ./dist",
+          "cli:build":
+            "node scripts/build-cli.js build && npm run templates:copy",
+          "cli:dev": "node scripts/build-cli.js dev",
+          dev: "npm run clean:build && npm-run-all --parallel cli:dev web:dev templates:dev",
+          package: "svelte-kit sync && svelte-package -o ./dist/web && publint",
+          "templates:copy": "cp -a ./src/cli/templates ./dist/cli",
+          "templates:dev":
+            "nodemon --watch src/cli/templates --ext '.ejs' --exec 'npm run templates:copy'",
+          "web:build": "vite build && npm run package",
+          "web:dev": "vite dev",
+        },
+        devDependencies: {
+          "@webstone/gluegun": "0.0.5",
+          "fs-jetpack": "^5.1.0",
+          nodemon: "^3.0.1",
+          "npm-run-all": "^4.1.5",
+          rimraf: "^3.0.2",
+          tsup: "^6.7.0",
+        },
+        svelte: "./dist/web/index.js",
+        types: "./dist/web/index.d.ts",
+        exports: {
+          ".": {
+            types: "./dist/web/index.d.ts",
+            svelte: "./dist/web/index.js",
+          },
+        },
+      },
+      {
+        encoding: "utf-8",
+        spaces: "\t",
+      },
+    ]);
+  });
+});
+
+describe("copyBuildScript", () => {
+  it("should copy the build script properly", (ctx) => {
+    const mockCopySync = ctx.mock.fn(fs.copySync, () => {});
+    ctx.mock.method(fs, "copySync", mockCopySync);
+    copyBuildScript("dummy");
+    strictEqual(mockCopySync.mock.callCount(), 1);
+    deepEqual(
+      // TODO find a solution for import.meta.url
+      mockCopySync.mock.calls[0].arguments[1],
+      "dummy/scripts/build-cli.js",
+    );
   });
 });
