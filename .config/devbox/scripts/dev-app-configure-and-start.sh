d166395ba356283cce596c2603b400bc3ac10895
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2023-08-20 23:33:15 -0700

    chore: Link plugins with the new structure to the _dev-app.

--- .config/devbox/scripts/dev-app-configure-and-start.sh
+++ .config/devbox/scripts/dev-app-configure-and-start.sh
@@ -4,12 +4,19 @@ set -e
 (
   if [ ! -d "_dev-app" ]
   then
-    node ./packages/create-webstone-app/bin.js _dev-app --type=application
+    node ./packages/create-webstone-app/bin.js _dev-app --type=app
   fi
   cd _dev-app
   echo "⏳ Waiting for the CLI's binary to be built..."
   while [ ! -f ../packages/cli/dist/bin.js ]; do sleep 1; done
   pnpm add -D ../packages/cli
+
+  # Install all plugin-* packages
+  for FILE in $(find ../packages/plugin-*/package.json)
+  do
+    DIR=`dirname $FILE`
+    pnpm add -D $DIR
+  done
 
   # Install all plugin-*/cli packages (without nested monorepo)
   # This is for the plugin-trpc package. If / when we migrate that to a monorepo, we can remove this code
