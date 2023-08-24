77dfdf9949cdf0c03f59f0eaaa571ade25a3e877
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2023-08-24 00:33:03 -0700

    Revert the manual export of types.

---
+++ .changeset/wet-coins-sell.md
@@ -0,0 +1,5 @@
+---
+"webstone-plugin-request-logger": patch
+---
+
+Revert v0.3.3, there is no need to export types manually.
--- packages/plugin-request-logger/src/lib/index.ts
+++ packages/plugin-request-logger/src/lib/index.ts
@@ -1,3 +1,2 @@
 // Reexport your entry components here
-export type * from './request-logger.js';
 export * from './request-logger.js';
