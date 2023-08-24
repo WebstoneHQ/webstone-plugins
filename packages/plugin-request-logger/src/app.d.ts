01ed8ecc9bd4ec72bf359f999c673b23fcc25f05
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2023-08-23 23:44:19 -0700

    chore: Export the RequestLogger interface. (#410)

--- packages/plugin-request-logger/src/app.d.ts
+++ packages/plugin-request-logger/src/app.d.ts
@@ -1,10 +1,13 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { RequestLogger } from '$lib/request-logger.ts';
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			logger: Console;
+			logger: RequestLogger;
 		}
 		// interface PageData {}
 		// interface Platform {}
--- packages/plugin-request-logger/src/lib/request-logger.ts
+++ packages/plugin-request-logger/src/lib/request-logger.ts
@@ -1,6 +1,53 @@
 import type { Handle } from '@sveltejs/kit';
 import { randomUUID } from 'node:crypto';
 
+export interface RequestLogger extends Console {
+	/**
+	 * Overrides the Console's debug function.
+	 * Logs a debug message with optional additional data.
+	 *
+	 * @param message - The debug message to log.
+	 * @param log_data - Optional additional data to include in the log.
+	 */
+	debug(message: string, log_data?: Record<string, unknown>): void;
+
+	/**
+	 * Overrides the Console's error function.
+	 * Logs an error message with optional additional data.
+	 *
+	 * @param message - The error message to log.
+	 * @param log_data - Optional additional data to include in the log.
+	 */
+	error(message: string, log_data?: Record<string, unknown>): void;
+
+	/**
+	 * Overrides the Console's info function.
+	 * Logs an info message with optional additional data.
+	 *
+	 * @param message - The info message to log.
+	 * @param log_data - Optional additional data to include in the log.
+	 */
+	info(message: string, log_data?: Record<string, unknown>): void;
+
+	/**
+	 * Overrides the Console's log function.
+	 * Logs a log message with optional additional data.
+	 *
+	 * @param message - The log message to log.
+	 * @param log_data - Optional additional data to include in the log.
+	 */
+	log(message: string, log_data?: Record<string, unknown>): void;
+
+	/**
+	 * Overrides the Console's warn function.
+	 * Logs a warning message with optional additional data.
+	 *
+	 * @param message - The warning message to log.
+	 * @param log_data - Optional additional data to include in the log.
+	 */
+	warn(message: string, log_data?: Record<string, unknown>): void;
+}
+
 export const addRequestLogger = (async ({ event, resolve }) => {
 	const request_id = randomUUID();
 
@@ -17,7 +64,7 @@ export const addRequestLogger = (async ({ event, resolve }) => {
 		return result;
 	}, {} as Console);
 
-	const logger = {
+	const logger: RequestLogger = {
 		...console,
 		...overwrites
 	};
